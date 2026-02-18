d8934cfe672de4d0fd76ed646a5b3006fc0a882a
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2026-02-18 21:39:56 +0530

    go concurrency

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goconcurrency
 A various use case and patterns for go concurrecny
+
+## Concurrency
+Simultaneously making progress on more than one task is known as concurrency.
---
+++ gocon.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Function 1")
+		wg.Done()
+	}()
+
+	go func() {
+		defer wg.Done()
+		fmt.Println("Function 2")
+
+	}()
+	wg.Wait()
+	fmt.Println("Execution Complete")
+}
